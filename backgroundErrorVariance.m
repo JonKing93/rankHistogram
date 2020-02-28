83a4ce69e50953dce80605855fbbea68d0d58d54
JonKing93 <jonking93@gmail.com>
2020-02-28 11:39:11 -0700

    finished initial build of backgroundErrorVariance

--- backgroundErrorVariance.m
+++ backgroundErrorVariance.m
@@ -1,16 +1,23 @@
-function[] = backgroundErrorVariance( D, Ye, Yf )
+function[sb2] = backgroundErrorVariance( D, Ye, Yf )
 %% Diagnoses background error variance as per Desroziers, 2005
 %
 % sb2 = backgroundErrorVariance( D, Ye, Yf )
 %
 % ----- Inputs -----
 %
-% D: Observations in a single time step (nSite x 1)
+% D: Observations (nSite x nTime)
 %
 % Ye: Prior estimates (nSite x nEns)
 %
-% Yf: Posterior estimates in a single time step (nSite x nEns x 1)
+% Yf: Posterior estimates (nSite x nEns x nTime)
 
-nObs = size(D,1);
-sb2 = nansum( (Yf-Ye) .* (D-Ye) / nObs );
+% Desroziers is for classical (non-ensemble) systems. Use the proxy means
+Ye = mean( Ye, 2 );
+Yf = squeeze( mean(Yf, 2) );
 
+% Get the background error variance in each time step for the sites with
+% observations
+nObs = sum( ~isnan(D), 1 );
+sb2 = nansum( (Yf-Ye) .* (D-Ye) ./ nObs, 1 )';
+
+end
